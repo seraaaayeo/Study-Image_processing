205a7df0a41e4337bd4b4be1ba97399b208c5345
Dasol <bluemoon18ds@gmail.com>
2020-05-09 23:50:37 +0900

    dct-idct src

---
+++ Project64/Project64/dct_samTable.c
@@ -0,0 +1,61 @@
+#include "dct3.h"
+
+void sampleDCT(double * sample, double * result)
+{
+	int i, j, Bx, By;
+
+	double arrSam[8][8] = {
+		{162, 162, 162, 161, 162, 156, 163, 160},
+	{162, 162, 162, 161, 162, 156, 163, 160},
+	{162, 162, 162, 161, 162, 156, 163, 160},
+	{162, 162, 162, 161, 162, 156, 163, 160},
+	{162, 162, 162, 161, 162, 156, 163, 160},
+	{164, 164, 157, 155, 161, 159, 158, 159},
+	{160, 160, 163, 158, 160, 161, 159, 155},
+	{158, 158, 155, 155, 158, 158, 156, 157}
+	};
+
+	for (By = 0; By < HEIGHT / N; By++)
+		for (Bx = 0; Bx < WIDTH / N; Bx++) {
+			for (i = 0; i < N; i++) {
+				for (j = 0; j < N; j++) {
+					*(result + (Bx * N) + (By * N * WIDTH) + i * WIDTH + j) =
+						(round((*(sample + (Bx * N) + (By * N * WIDTH) + i * WIDTH + j)) / arrSam[i][j])) * arrSam[i][j];
+
+	/*
+	double *arrSam = (double*)malloc(sizeof(double) * 8 * 8);
+	for (int i = 0; i < 8; i++) {
+		for (int j = 0; j < 8; j++) {
+			*(arrSam + 8 * i + j) = arrSam[i][j];
+		}
+	}
+	*//*
+	for (By = 0; By < N / N; By++)
+		for (Bx = 0; Bx < N / N; Bx++) {
+			for (u = 0; u < N; u++) {
+				if (u == 0)
+					c_u = (1 / sqrt(2));
+				else
+					c_u = 1;
+				for (v = 0; v < N; v++) {
+					if (v == 0)
+						c_v = (1 / sqrt(2));
+					else
+						c_v = 1;
+					sum = 0;
+					for (i = 0; i < N; i++) {
+						for (j = 0; j < N; j++) {
+							sum +=
+								cos((2 * i + 1) * u * pi / (2 * N)) * cos((2 * j + 1) * v * pi / (2 * N))*
+								(*(input + (Bx * N) + (By * N * WIDTH) + i * WIDTH + j));
+						}
+					}
+					*(output + u * WIDTH + v) = (c_u * c_v) / (sqrt(N * N) / 2) * sum;	
+					*/
+				}
+			}
+		}
+}
+
+		
+
---
+++ Project64/Project64/psnr.c
@@ -0,0 +1,22 @@
+#include "dct3.h"
+
+void psnr(unsigned char * input, unsigned char * output)
+{
+	/*	PSNR(X,Y)=10log((X_max)^2 / MSE(X,Y)
+		X 원본 영상 Y 처리된 영상 X_max 최대 가능 픽셀 수(최대픽셀값-최소픽셀값)
+		MSE(Mean Square Error) 평균 제곱 오차 = (통계학)표준편차
+		MSE = 1/N (i=0 ~ i=n-1)sum(Xi-Yi)^2						*/
+
+	double PSNR;
+	double sum=0, MSE=0;
+	
+	for (int m = 0; m < HEIGHT; m++) {
+		for (int n = 0; n < WIDTH; n++) {
+			sum += (*(input + m * WIDTH + n) - *(output + m * WIDTH + n))*(*(input + m * WIDTH + n) - *(output + m * WIDTH + n));
+		}
+	}
+	MSE = sum / (HEIGHT*WIDTH);
+	PSNR = 10 * log10(255 * 255 / MSE);		////unsigned char의 범위 0~255이므로 (255-0)*(255-0)
+	printf("error rate: %lf\n", MSE);
+	printf("PSNR : %lf\n", PSNR);
+}
