333dca34cc46a4ea7da8bf3ed9cc26f689b213c7
Dasol <bluemoon18ds@gmail.com>
2020-05-12 20:48:02 +0900

    img rotate with pointer

--- src/img_pointer/Project62/main_pointer.c
+++ src/img_pointer/Project62/main_pointer.c
@@ -18,38 +18,38 @@ int main(void)
 	new_img = (unsigned char*)malloc(sizeof(unsigned char)*HEIGHT*WIDTH);
 	
 	printf("--------------------------------	Menu	----------------------------------------\n");
-	printf("1.���Ϲ��� 2.�¿���� 3.90�� ȸ�� 4.180�� ȸ�� 5.270�� ȸ�� 6.360�� ȸ��\n");
+	printf("1.상하반전 2.좌우반전 3.90도 회전 4.180도 회전 5.270도 회전 6.360도 회전\n");
 	printf("--------------------------------	Menu	----------------------------------------\n");
 
 	printf("choose a number ");
 	scanf("%d", &menu);
 
-	FILE * infile = fopen("F16.img", "rb");		//�̹��� ������ �б� ���� �ҷ�����
+	FILE * infile = fopen("F16.img", "rb");		//이미지 파일을 읽기 모드로 불러오기
 	if (infile == NULL) {
 		printf("file open error!");
 		return 1;
 	}
 
-	fread(org_img, sizeof(char), HEIGHT*WIDTH, infile);		//�̹����κ��� �ȼ� ���� �б�
+	fread(org_img, sizeof(char), HEIGHT*WIDTH, infile);		//이미지로부터 픽셀 정보 읽기
 
 	fclose(infile);
 
 	if (menu == 1) {
-		///		���Ϲ���	 ///
+		///		상하반전	 ///
 		for (int i = 0; i < HEIGHT; i++) {
 			for (int j = 0; j < WIDTH; j++) {
 				*(new_img + WIDTH * i + j) = *(org_img + WIDTH*(HEIGHT-i-1) + j);
 			}
 		}
 	
-		FILE * outfile = fopen("F16_UpDown.img", "wb");		//��� ������ ���� ���� ����
-		fwrite(new_img, sizeof(char), HEIGHT*WIDTH, outfile);		//��� �̹����� �ȼ� ���� ����
+		FILE * outfile = fopen("F16_UpDown.img", "wb");		//출력 파일을 쓰기 모드로 열기
+		fwrite(new_img, sizeof(char), HEIGHT*WIDTH, outfile);		//결과 이미지에 픽셀 정보 저장
 		fclose(outfile);
 		free(new_img);		//���� �Ҵ��� �޸��� ���ҽ� ����
 	}
 	
 	else if (menu == 2) {
-		////	�¿����	///
+		////	좌우반전	///
 		for (int i = 0; i < HEIGHT; i++) {
 			for (int j = 0; j < WIDTH; j++) {
 				*(new_img + WIDTH * i + j) = *(org_img + WIDTH * i + (WIDTH - j - 1));
@@ -57,13 +57,13 @@ int main(void)
 		}
 
 		FILE*outfile = fopen("F16_LeftRight.img", "wb");
-		fwrite(new_img, sizeof(char), HEIGHT*WIDTH, outfile);		//��� �̹����� �ȼ� ���� ����
+		fwrite(new_img, sizeof(char), HEIGHT*WIDTH, outfile);		
 		fclose(outfile);
 		free(new_img);
 	}
 	
 	else if (menu == 3) {
-		////	90�� ȸ��	///
+		////	90도 회전	///
 		for (int j = 0; j < WIDTH; j++) {
 			for (int i = 0; i < HEIGHT; i++) {
 				*(new_img + HEIGHT*j + i) = *(org_img + WIDTH * (HEIGHT - i - 1) + j);
@@ -71,13 +71,13 @@ int main(void)
 		}
 		
 		FILE*outfile = fopen("F16_rot_90.img", "wb");
-		fwrite(new_img, sizeof(char), HEIGHT*WIDTH, outfile);		//��� �̹����� �ȼ� ���� ����
+		fwrite(new_img, sizeof(char), HEIGHT*WIDTH, outfile);		
 		fclose(outfile);
 		free(new_img);
 	}
 	
 	else if (menu == 4) {
-		///////////		180�� ȸ�� = 90�� ȸ�� * 2	///////////
+		///////////		180도 회전 = 90도 회전 * 2	///////////
 		int i, j;
 		unsigned char * temp;
 		temp = (unsigned char*)malloc(sizeof(unsigned char)*HEIGHT*WIDTH);
@@ -93,13 +93,13 @@ int main(void)
 			}
 		}
 		FILE*outfile = fopen("F16_rot_180.img", "wb");
-		fwrite(new_img, sizeof(char), HEIGHT*WIDTH, outfile);		//��� �̹����� �ȼ� ���� ����
+		fwrite(new_img, sizeof(char), HEIGHT*WIDTH, outfile);		
 		fclose(outfile);
 		free(new_img);
 	}
 	
 	else if (menu == 5) {
-		///////////		270�� ȸ�� = 90�� ȸ�� * 3	///////////
+		///////////		270도 회전 = 90도 회전 * 3	///////////
 		int i, j;
 		unsigned char * temp, *temp2;
 		temp = (unsigned char*)malloc(sizeof(unsigned char)*HEIGHT*WIDTH);
@@ -123,13 +123,13 @@ int main(void)
 			}
 		}
 		FILE*outfile = fopen("F16_rot_270.img", "wb");
-		fwrite(new_img, sizeof(char), HEIGHT*WIDTH, outfile);		//��� �̹����� �ȼ� ���� ����
+		fwrite(new_img, sizeof(char), HEIGHT*WIDTH, outfile);		
 		fclose(outfile);
 		free(new_img);
 	}
 	
 	else if (menu == 6) {
-		///////////		360�� ȸ�� = 90�� ȸ�� * 4	///////////
+		///////////		360도 회전 = 90도 회전 * 4	///////////
 		int i, j;
 
 		unsigned char * temp, *temp2, *temp3;
@@ -161,10 +161,10 @@ int main(void)
 			}
 		}
 		FILE*outfile = fopen("F16_rot_360.img", "wb");
-		fwrite(new_img, sizeof(char), HEIGHT*WIDTH, outfile);		//��� �̹����� �ȼ� ���� ����
+		fwrite(new_img, sizeof(char), HEIGHT*WIDTH, outfile);		
 		fclose(outfile);
 		free(new_img);
 	}
 	
-	printf("�̹��� ��ȯ �Ϸ�");
+	printf("이미지 변환 완료");
 }
